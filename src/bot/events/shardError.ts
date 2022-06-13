5ea20f620dfbf1a48ed18b1665aad51536688a98
Shadow <william.shadow@outlook.com>
2022-06-13 11:26:05 -0500

    ope

--- src/bot/events/shardError.ts
+++ src/bot/events/shardError.ts
@@ -4,7 +4,7 @@ export default class ShardError extends EventHandler {
     override async run(error: Error, shardId: number) {
         this.client.logger.error(error)
         this.client.logger.sentry.captureWithExtras(error, { Shard: shardId })
-        const haste = this.client.functions.uploadHaste(`${error.name}: ${error.message}`)
+        const haste = await this.client.functions.uploadHaste(`${error.name}: ${error.message}`)
         return this.client.logger.webhookLog("console", {
             content: `${this.client.functions.generateTimestamp()} Shard ${this.client.shard?.ids[0]} encountered an error: ${haste}`,
         })
