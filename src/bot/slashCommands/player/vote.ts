f506424b290f0cc6a25beb6eb3a912e54b09ad19
Shadow <william.shadow@outlook.com>
2022-12-31 03:44:11 +0000

    fixed it

--- src/bot/slashCommands/player/vote.ts
+++ src/bot/slashCommands/player/vote.ts
@@ -2,7 +2,7 @@ import { CommandInteraction, TextBasedChannel } from "discord.js"
 import SlashCommand from "../../../../lib/classes/SlashCommand"
 import BetterClient from "../../../../lib/extensions/BlobbyClient"
 
-export default class Ping extends SlashCommand {
+export default class Vote extends SlashCommand {
     constructor(client: BetterClient) {
         super("me", client, {
             description: `Vote for a player`,
