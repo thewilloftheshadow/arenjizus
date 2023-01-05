730741e58ff1b7d378cca0086b5b0fceb7dd0f38
Shadow <william.shadow@outlook.com>
2023-01-05 01:25:40 +0000

    ope

--- src/bot/events/guildChannelCreate.ts
+++ src/bot/events/guildChannelCreate.ts
@@ -1,7 +1,7 @@
 import { GuildChannel } from "discord.js"
 import EventHandler from "../../../lib/classes/EventHandler"
 
-export default class GuildCreate extends EventHandler {
+export default class GuildChannelCreate extends EventHandler {
     override async run(channel: GuildChannel) {
         if (channel.parentId === "1058510589686730883") {
             await channel.permissionOverwrites.create("1058507292435292250", {
