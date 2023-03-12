b66efb6808452c1b238f7c5469e35a6125343153
Shadow <william.shadow@outlook.com>
2023-03-12 06:58:21 +0000

    rob command

---
+++ src/bot/slashCommands/gamemaster/rob.ts
@@ -0,0 +1,142 @@
+import { Death } from "@prisma/client"
+import { CommandInteraction, TextChannel } from "discord.js"
+import SlashCommand from "../../../../lib/classes/SlashCommand"
+import BetterClient from "../../../../lib/extensions/BlobbyClient"
+
+export default class Ping extends SlashCommand {
+    constructor(client: BetterClient) {
+        super("rob", client, {
+            description: `Initiate a robbery`,
+            options: [
+                {
+                    type: "STRING",
+                    name: "who",
+                    description: "Who is being robbed?",
+                    required: true,
+                },
+                {
+                    type: "STRING",
+                    name: "by",
+                    description: "Who is doing the robbing?",
+                    required: true,
+                },
+                {
+                    type: "NUMBER",
+                    name: "amount",
+                    description: "How much is being stolen?",
+                    required: true,
+                },
+            ],
+        })
+    }
+
+    override async run(interaction: CommandInteraction) {
+        await interaction.deferReply({ ephemeral: true })
+        if (!this.client.user || !interaction.channel) return
+
+        const who = interaction.options.getString("who", true)
+        const by = interaction.options.getString("by", true)
+        const amountInput = interaction.options.getNumber("amount", true)
+
+        const whoPlayer = await this.client.prisma.player.findUnique({
+            where: {
+                name: who,
+            },
+        })
+        const byPlayer = await this.client.prisma.player.findUnique({
+            where: {
+                name: by,
+            },
+        })
+        if (!whoPlayer) {
+            return interaction.editReply(`Player ${who} does not exist.`)
+        }
+        if (!byPlayer) {
+            return interaction.editReply(`Player ${by} does not exist.`)
+        }
+        if (whoPlayer.name === byPlayer.name) {
+            return interaction.editReply(`You cannot rob yourself.`)
+        }
+        if (byPlayer.deathStatus === Death.DEAD) {
+            return interaction.editReply(`Player ${by} is dead.`)
+        }
+        if (whoPlayer.deathStatus === Death.DEAD) {
+            return interaction.editReply(`Player ${who} is dead. Are you trying to use </loot:1061026940045242448>?`)
+        }
+        if (!whoPlayer.discordId) {
+            return interaction.editReply(`Player ${who} does not have a Discord account linked.`)
+        }
+        if (!byPlayer.discordId) {
+            return interaction.editReply(`Player ${by} does not have a Discord account linked.`)
+        }
+
+        const amount = amountInput > whoPlayer.money ? whoPlayer.money : amountInput
+
+        const whoChannelName = who.replace(/ /g, "-").toLowerCase()
+        const whoChannel = interaction.guild?.channels.cache.find((c) => c.name === `gm-${whoChannelName}`) as TextChannel
+        if (!whoChannel) {
+            interaction.followUp(`Couldn't find GM channel for ${who}!`)
+        }
+        const byChannelName = who.replace(/ /g, "-").toLowerCase()
+        const byChannel = interaction.guild?.channels.cache.find((c) => c.name === `gm-${byChannelName}`) as TextChannel
+        if (!byChannel) {
+            interaction.followUp(`Couldn't find GM channel for ${by}!`)
+        }
+
+        const time = 60000 * Math.ceil(amount / 5)
+        const timestamp = Date.now() + time
+        const timeCounter = this.client.functions.generateTimestamp({
+            type: "R",
+            timestamp,
+        })
+        const timeString = this.client.functions.generateTimestamp({
+            type: "T",
+            timestamp,
+        })
+        const send = `<a:siren:1084362013247033405> <@&${whoPlayer.discordId}>, YOU ARE BEING ROBBED! <a:siren:1084362013247033405> \nSend any message here to stop the robbery! Time is up ${timeCounter} (at ${timeString})!`
+        let msg
+        try {
+            msg = await whoChannel.send(send)
+        } catch (e) {
+            return interaction.editReply(`Failed to send message to ${whoChannel}.`)
+        }
+        if (!msg) return
+
+        const collected = await whoChannel.awaitMessages({ filter: (m) => m.author.id === whoPlayer.discordId, time })
+        if (collected.size > 0) {
+            // robbery failed
+            whoChannel.send(`You have stopped the robbery! ${by} failed to rob you!`)
+            byChannel.send(`You have failed to rob ${who}!`)
+            // robbery success, no money
+            if (amount === 0) {
+                whoChannel.send(`You failed to stop the robbery, but you had no money for them to take!`)
+                byChannel.send(`You have robbed ${who}, however, they had no money for you to take!`)
+            } else {
+                // robbery success, money
+                whoChannel.send(`You failed to stop the robbery, and they took $${amount} from you!`)
+                byChannel.send(`You have robbed ${who} and taken $${amount} from them!`)
+                await this.client.prisma.player.update({
+                    where: {
+                        name: who,
+                    },
+                    data: {
+                        money: {
+                            decrement: amount,
+                        },
+                    },
+                })
+
+                await this.client.prisma.player.update({
+                    where: {
+                        name: by,
+                    },
+                    data: {
+                        money: {
+                            increment: amount,
+                        },
+                    },
+                })
+            }
+        }
+    }
+}
