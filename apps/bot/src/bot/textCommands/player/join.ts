3a040b3b014b019b2c5335a1108311652f119f53
Shadow <hi@shadowing.dev>
2023-05-10 02:21:43 -0500

    add check

--- apps/bot/src/bot/textCommands/player/join.ts
+++ apps/bot/src/bot/textCommands/player/join.ts
@@ -8,6 +8,7 @@ export default class Cmd extends TextCommand {
 	}
 
 	override async run(message: Message) {
+		if (message.channelId !== "1105754920063352852") return message.reply("LMAO get a load of this nerd")
 		await message.delete()
 		message.member?.roles.add(serverIds.roles.player)
 	}
