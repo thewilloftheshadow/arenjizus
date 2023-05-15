64f44a78d9384031f1b10287b0162c911bc57339
Shadow <hi@shadowing.dev>
2023-05-15 13:57:57 -0500

    ahhh

--- packages/functions/src/player.ts
+++ packages/functions/src/player.ts
@@ -6,3 +6,4 @@ export const getPlayerChannel = async (name: string, client: Client) => {
 	const guild = client.guilds.cache.get(serverIds.guild)
 	return guild?.channels.cache.find((c) => c.name === `gm-${name.toLowerCase().replace(/ /g, "-")}`) as TextChannel | null
 }
+	
