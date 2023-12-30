948d57e4683c03e4d16b8c3b2a634a33468afb42
Shadow <shadow@buape.com>
2023-12-30 14:31:08 -0600

    say command

---
+++ apps/bot/src/bot/applicationCommands/gamemaster/say.ts
@@ -0,0 +1,64 @@
+import { ApplicationCommand } from "@buape/lib"
+import { BetterClient } from "@buape/lib"
+import { generateErrorMessage } from "@internal/functions"
+import {
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	ChannelType,
+	ChatInputCommandInteraction
+} from "discord.js"
+import { ApplicationCommandOptionType } from "discord.js"
+
+export default class Ping extends ApplicationCommand {
+	constructor(client: BetterClient) {
+		super("say", client, {
+			description: `Make me say something`,
+			options: [
+				{
+					type: ApplicationCommandOptionType.String,
+					name: "text",
+					description: "The text to say",
+					required: true
+				},
+				{
+					type: ApplicationCommandOptionType.Channel,
+					name: "channel",
+					description: "The channel to send it in"
+				}
+			]
+		})
+	}
+
+	override async run(interaction: ChatInputCommandInteraction) {
+		await interaction.deferReply({ ephemeral: true })
+		const text = interaction.options.getString("text", true)
+		const channel = await interaction.guild?.channels.fetch(
+			interaction.options.getChannel("channel", false)?.id ||
+				interaction.channelId
+		)
+
+		if (channel?.type !== ChannelType.GuildText) {
+			return interaction.editReply(
+				generateErrorMessage({
+					title: "Invalid channel",
+					description: `The channel you specified is not a text channel`
+				})
+			)
+		}
+
+		const msg = await channel.send(text)
+
+		return interaction.editReply({
+			content: "Sent!",
+			components: [
+				new ActionRowBuilder<ButtonBuilder>().addComponents(
+					new ButtonBuilder()
+						.setURL(msg.url)
+						.setLabel("View message")
+						.setStyle(ButtonStyle.Link)
+				)
+			]
+		})
+	}
+}
