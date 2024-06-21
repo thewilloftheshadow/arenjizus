97cf19c34eefcf49a62b0c9878eb4804a684f263
Shadow <hi@shadowing.dev>
2024-06-21 14:00:49 -0400

    backup

---
+++ apps/bot/src/bot/applicationCommands/gamemaster/dump.ts
@@ -0,0 +1,26 @@
+import { ApplicationCommand } from "@buape/lib"
+import type { BetterClient } from "@buape/lib"
+import type { ChatInputCommandInteraction } from "discord.js"
+import { copyFileSync } from "node:fs"
+
+export default class Ping extends ApplicationCommand {
+	constructor(client: BetterClient) {
+		super("backup", client, {
+			description: `Take a backup of the current state of the db for future rollback`
+		})
+	}
+
+	override async run(interaction: ChatInputCommandInteraction) {
+		await interaction.deferReply()
+		if (!this.client.__dirname.startsWith("/home/ubuntu/arenjizus"))
+			return interaction.editReply(
+				"This command can only be used the production environment (yay jank code)"
+			)
+		const newName = `backup_${Date.now()}.db`
+		copyFileSync(
+			`/home/ubuntu/arenjizus/packages/database/dev.db`,
+			`/home/ubuntu/arenjizus/packages/database/${newName}`
+		)
+		return interaction.editReply(`Backup created as ${newName}`)
+	}
+}
