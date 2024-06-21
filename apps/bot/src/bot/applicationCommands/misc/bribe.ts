bba186bd259f72d082866433059abf9972f26d25
Shadow <hi@shadowing.dev>
2024-06-21 14:44:34 -0400

    bribe

---
+++ apps/bot/src/bot/applicationCommands/misc/bribe.ts
@@ -0,0 +1,16 @@
+import { ApplicationCommand, type BetterClient } from "@buape/lib"
+import type { ChatInputCommandInteraction } from "discord.js"
+
+export default class Command extends ApplicationCommand {
+	constructor(client: BetterClient) {
+		super("bribe", client, {
+			description: ":D"
+		})
+	}
+
+	override async run(interaction: ChatInputCommandInteraction) {
+		return interaction.reply(
+			`The <@&1105539807444217874>s are very bribable! :D\nJust list who you want the money to go to at https://ko-fi.com/theshadow`
+		)
+	}
+}
