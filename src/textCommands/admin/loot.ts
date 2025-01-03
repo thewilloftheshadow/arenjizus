fe0004d25cce2212a55169fccb372a2014e694c4
Shadow <hi@shadowing.dev>
2025-01-03 14:15:52 -0600

    oops

--- src/textCommands/admin/loot.ts
+++ src/textCommands/admin/loot.ts
@@ -3,9 +3,9 @@ import database from "~/database"
 import { grantAbility } from "~/database/ability"
 import { type BetterClient, TextCommand } from "~/lib"
 
-export default class Kill extends TextCommand {
+export default class Loot extends TextCommand {
 	constructor(client: BetterClient) {
-		super("kill", client, {})
+		super("loot", client, {})
 	}
 
 	override async run(message: Message) {
