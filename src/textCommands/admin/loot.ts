1019796fea913d5a79fb5fcb9f23c55c53f9e536
Shadow <hi@shadowing.dev>
2025-01-03 14:15:31 -0600

    add loot cmd

---
+++ src/textCommands/admin/loot.ts
@@ -0,0 +1,25 @@
+import type { Message } from "discord.js"
+import database from "~/database"
+import { grantAbility } from "~/database/ability"
+import { type BetterClient, TextCommand } from "~/lib"
+
+export default class Kill extends TextCommand {
+	constructor(client: BetterClient) {
+		super("kill", client, {})
+	}
+
+	override async run(message: Message) {
+		if (
+			![
+				"960888903764676618",
+				"389840562112561183",
+				"439223656200273932"
+			].includes(message.author.id)
+		)
+			return
+		if (!message.channel.isSendable()) return
+		const players = await database.player.findMany()
+		players.map((a) => grantAbility(a.name, "Loot"))
+		message.reply("Loot ability granted to all players")
+	}
+}
