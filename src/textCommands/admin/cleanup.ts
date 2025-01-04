37b93105dc6ba507dbdcc39c9358426031164d5a
Shadow <hi@shadowing.dev>
2025-01-04 11:49:09 -0600

    cleanup cmd

---
+++ src/textCommands/admin/cleanup.ts
@@ -0,0 +1,40 @@
+import type { Message } from "discord.js"
+import database from "~/database"
+import { type BetterClient, TextCommand } from "~/lib"
+
+export default class Loot extends TextCommand {
+	constructor(client: BetterClient) {
+		super("loot", client, {})
+	}
+
+	override async run(message: Message) {
+		if (
+			![
+				"960888903764676618",
+				"389840562112561183",
+				"439223656200273932"
+			].includes(message.author.id)
+		)
+			return
+		if (!message.channel.isSendable()) return
+		const done = await database.playerAbilities.deleteMany({
+			where: {
+				usesLeft: 0,
+				ability: {
+					customOneOff: true
+				}
+			}
+		})
+		const done2 = await database.ability.deleteMany({
+			where: {
+				customOneOff: true,
+				playersWithAbility: {
+					none: {}
+				}
+			}
+		})
+		message.reply(
+			`Cleaned up ${done} playerAbility entries and ${done2} ability entries`
+		)
+	}
+}
