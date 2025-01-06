172a25e502294945ce217e50fa82f9a59b8545ab
Shadow <hi@shadowing.dev>
2025-01-06 14:20:13 -0600

    ahah

--- src/applicationCommands/gamemaster/checkalive.ts
+++ src/applicationCommands/gamemaster/checkalive.ts
@@ -1,4 +1,8 @@
-import { ChannelType, type ChatInputCommandInteraction } from "discord.js"
+import {
+	ApplicationCommandOptionType,
+	ChannelType,
+	type ChatInputCommandInteraction
+} from "discord.js"
 import { serverIds } from "~/config"
 import { getAllPlayers } from "~/database/getData"
 import { ApplicationCommand } from "~/lib"
@@ -7,7 +11,15 @@ import type { BetterClient } from "~/lib"
 export default class Ping extends ApplicationCommand {
 	constructor(client: BetterClient) {
 		super("checkalive", client, {
-			description: `Check alive players for mutual channels`
+			description: `Check alive players for mutual channels`,
+			options: [
+				{
+					name: "all",
+					description: "Require all players to be alive",
+					type: ApplicationCommandOptionType.Boolean,
+					required: false
+				}
+			]
 		})
 	}
 
@@ -16,6 +28,8 @@ export default class Ping extends ApplicationCommand {
 		if (!interaction.guild)
 			return interaction.editReply("This command can only be used in a server")
 
+		const all = interaction.options.getBoolean("all")
+
 		const players = await getAllPlayers()
 		const filtered = players.filter((x) => x.isAlive && x.discordId !== null)
 
@@ -23,7 +37,14 @@ export default class Ping extends ApplicationCommand {
 			.filter((x) => x.type === ChannelType.GuildText)
 			.filter((x) => serverIds.inGameCategories.includes(x.parentId ?? ""))
 			.filter((x) => {
-				return filtered.every((member) =>
+				if (all)
+					return filtered.every((member) =>
+						x
+							// biome-ignore lint/style/noNonNullAssertion: hrm
+							.permissionsFor(member.discordId!)
+							?.has("ViewChannel")
+					)
+				return filtered.some((member) =>
 					x
 						// biome-ignore lint/style/noNonNullAssertion: hrm
 						.permissionsFor(member.discordId!)
@@ -33,7 +54,7 @@ export default class Ping extends ApplicationCommand {
 			.filter((x) => x.name !== "instructions")
 
 		return interaction.editReply(
-			`Found ${channels.size} alliance channels that all alive players have access to:\n${channels
+			`Found ${channels.size} alliance channels that ${all ? "all" : "at least some"} alive players have access to:\n${channels
 				.map((x) => `<#${x.id}>`)
 				.join("\n")}`
 		)
