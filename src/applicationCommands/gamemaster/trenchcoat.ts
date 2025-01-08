40000b80018cf24878f5ab1e9bc7e92363a1cdbd
Shadow <hi@shadowing.dev>
2025-01-08 12:58:02 -0600

    trenchcoat command

---
+++ src/applicationCommands/gamemaster/trenchcoat.ts
@@ -0,0 +1,82 @@
+import {
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	ChannelType,
+	type ChatInputCommandInteraction
+} from "discord.js"
+import { ApplicationCommandOptionType } from "discord.js"
+import { generateErrorMessage } from "~/functions/generateMessage"
+import { ApplicationCommand } from "~/lib"
+import type { BetterClient } from "~/lib"
+
+export default class Ping extends ApplicationCommand {
+	constructor(client: BetterClient) {
+		super("trenchcoat", client, {
+			description: `Yay trenchcoat`,
+			options: [
+				{
+					type: ApplicationCommandOptionType.String,
+					name: "text",
+					description: "The text to say",
+					required: true
+				},
+				{
+					type: ApplicationCommandOptionType.Channel,
+					name: "channel",
+					description: "The channel to send it in"
+				}
+			]
+		})
+	}
+
+	override async run(interaction: ChatInputCommandInteraction) {
+		await interaction.deferReply({ ephemeral: true })
+		const text = interaction.options.getString("text", true)
+		const channel = await interaction.guild?.channels.fetch(
+			interaction.options.getChannel("channel", false)?.id ||
+				interaction.channelId
+		)
+
+		if (
+			!channel ||
+			!channel.isSendable() ||
+			channel.type !== ChannelType.GuildText
+		) {
+			return interaction.editReply(
+				generateErrorMessage({
+					title: "Invalid channel",
+					description: `The channel you specified is not a text channel`
+				})
+			)
+		}
+
+		const hooks = await channel.fetchWebhooks()
+		let hook = hooks.find((x) => x.owner?.id === this.client.user?.id)
+		if (!hook) {
+			hook = await channel.createWebhook({ name: "Arenjizus" })
+		}
+
+		hook.send({
+			content: `${text}`,
+			username: "Trenchcoat",
+			avatarURL:
+				"https://cdnd.lystit.com/photos/2012/06/01/burberry-mercury-midlength-heritage-cotton-trench-coat-product-1-3824627-326438071.jpeg",
+			allowedMentions: {}
+		})
+
+		const msg = await channel.send(text)
+
+		return interaction.editReply({
+			content: "Sent!",
+			components: [
+				new ActionRowBuilder<ButtonBuilder>().addComponents(
+					new ButtonBuilder()
+						.setURL(msg.url)
+						.setLabel("View message")
+						.setStyle(ButtonStyle.Link)
+				)
+			]
+		})
+	}
+}
