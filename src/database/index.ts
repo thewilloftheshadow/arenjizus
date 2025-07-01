54986df26dcb5527821a81b2c68061437f26153e
Shadow <hi@shadowing.dev>
2025-07-01 15:53:21 -0500

    custom kiai domain

--- src/database/index.ts
+++ src/database/index.ts
@@ -8,5 +8,9 @@ export default database
 export * from "@prisma/client"
 
 export const kiai = new KiaiClient(
-	"aGl0aGVyZTU3NDAxMDQyOTYwMTIxODU2MA==.iOMIA83uuHFSL4p4i1X6LJOgCD6YVU"
+	"aGl0aGVyZTU3NDAxMDQyOTYwMTIxODU2MA==.iOMIA83uuHFSL4p4i1X6LJOgCD6YVU",
+	{
+		baseURL: "https://api-v1.kiai.app",
+		fetchFunction: fetch
+	}
 )
